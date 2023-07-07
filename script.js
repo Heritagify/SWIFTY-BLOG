23abf827ed22efe342b22c5bfdfc9701f625cb91
Heritageify <heritageolaiy@gmail.com>
2023-07-07 17:47:59 +0100

    did some media queries

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert("whatsup booo")
+document.getElementById("Open").addEventListener("click", ()=> {
+    document.querySelector(".nav-link").style.display= "flex";
+    document.getElementById("Open").style.display = "none";
+    document.getElementById("Close").style.display = "block";
+})
+
+
+document.getElementById("Close").addEventListener("click", ()=> {
+    document.querySelector(".nav-link").style.display= "none";
+    document.getElementById("Open").style.display = "inline-block";
+    document.getElementById("Close").style.display = "none";
+})
